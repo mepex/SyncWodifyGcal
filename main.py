f1e78869123b77a3eb10527813584a9157cde04c
mepex <mike.peattie@gmail.com>
2025-07-13 09:39:47 -0700

    Cleanup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import datetime
 import json
 
-from pytz import timezone, utc
+from pytz import timezone
 import os.path
 import argparse
 import time
@@ -10,13 +10,11 @@ import time
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build, Resource
+from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
-
-***REMOVED*** = "***REMOVED***"
 
 with open('settings.json', 'r') as file:
     settings = json.load(file)
@@ -36,15 +34,10 @@ def get_wodify_classes() -> dict:
     wodify_url = "https://api.wodify.com/v1/classes/search"
     now = datetime.datetime.now().isoformat()
     today = str(datetime.date.today())
-    headers = {"accept": "application/json", "x-api-key": ***REMOVED***}
+    headers = {"accept": "application/json", "x-api-key": settings["wodify_api_key"]}
     q = {"q": f"start_date|gte|{today};coach|eq|'{settings['wodify_me']}'", "sort": "start_date"}
     response = requests.get(wodify_url, headers=headers, params=q)
     return json.loads(response.text)
-
-def find_gcal_entry(date: str, description: str) -> dict:
-    global settings
-    now = datetime.datetime.now(tz=datetime.timezone.utc)
-
 
 
 # https://developers.google.com/workspace/calendar/api/v3/reference/events#resource
@@ -70,7 +63,7 @@ def find_gcal_entry(events: dict, start: datetime, description: str) -> dict:
         try:
             gcal_start = datetime.datetime.fromisoformat(entry['start']['dateTime'])
             gcal_iso = gcal_start.isoformat()
-            if gcal_start == start and entry['summary'] == description:
+            if gcal_start == start and description in entry['summary']:
                 found_entry = entry
                 break
         except KeyError as error:
@@ -89,21 +82,22 @@ def delete_declined(service, events):
                     print(events_result)
                 time.sleep(0.5)
 
-def delete_cfc_events(service, prefix, events):
+def delete_wodify_events(service, prefix, events):
+    deleted = 0
     for event in events:
         if prefix in event["summary"]:
             start = event["start"].get("dateTime", event["start"].get("date"))
             print(f'Deleting {start} -- {event["summary"]}')
             if not settings['print_only']:
                 events_result = service.events().delete(calendarId='primary', eventId=event['id']).execute()
+                deleted += 1
                 if events_result:
                     print(events_result)
                 time.sleep(0.5)
+    print(f'Deleted {deleted} events')
 
 def main():
     classes = get_wodify_classes()
-
-
 
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
@@ -151,38 +145,61 @@ def main():
             print("No upcoming events found.")
             return
 
-        parser = argparse.ArgumentParser(description="A simple script with command-line arguments.")
+        parser = argparse.ArgumentParser(description="Sync wodify coaching events to Google Calendar")
         parser.add_argument("--delete-declined", help="Clear GCal of declined events")
-        parser.add_argument("--delete-cfc", help="Clear GCal of CFC events")
+        parser.add_argument("--delete-wodify-events", help=f"Clear GCal of '{settings['prefix']}' events")
 
         args = parser.parse_args()
 
         if args.delete_declined:
             delete_declined(service, events)
             return
-        if args.delete_cfc:
-            delete_cfc_events(service, events)
+        if args.delete_wodify_events:
+            delete_wodify_events(service, events)
             return
 
         classtimes = []
         tz = timezone(settings['timezone'])
 
+        #
+        #  First, add Wodify classes to Gcal that don't already exist
+        #
+        created = 0
         for c in classes['classes']:
             dt = c['start_date_time']
-            # Force to timezone, all wodify classes are in UTC
+            # Force to timezone, all Wodify classes are in UTC
             start = tz.localize(datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ"))
-            classtimes.append(start)
+            classtimes.append(start.isoformat())
             name = f"{settings['prefix']}{c['name']}"
             found_entry_next = find_gcal_entry(events, start, name)
             if not found_entry_next:
-                print(f"Creating new gcal entry : {start.isoformat()} {name}")
+                print(f"New GCal event : {start.isoformat()} {name}")
                 event = make_gcal_entry(tz, c)
-                print(event)
+                #print(event)
                 if not settings['print_only']:
                     event = service.events().insert(calendarId='primary', body=event).execute()
+                    created += 1
             else:
-                print(f"Found existing gcal entry : {start.isoformat()} {name}")
+                print(f"Found existing GCal event : {start.isoformat()} {name}")
+        print(f"Created {created} new GCal events")
 
+        #
+        # Last, check for Wodify entries in GCal that aren't in Wodify and delete them
+        #
+        deleted = 0
+        for event in events:
+            try:
+                gcal_start = datetime.datetime.fromisoformat(event['start']['dateTime']).isoformat()
+            except KeyError as error:
+                continue
+            if settings["prefix"] in event["summary"] and gcal_start not in classtimes:
+                print(f"Deleting : {gcal_start} {event["summary"]}")
+                if not settings['print_only']:
+                    events_result = service.events().delete(calendarId='primary', eventId=event['id']).execute()
+                    deleted += 1
+                    if events_result:
+                        print(events_result)
+        print(f"Deleted {deleted} GCal events")
 
 
     except HttpError as error:
